2a3c2264aaa7b873024c47d5b87950e96fb87798
Kayla Nesbitt <77808103+kaylanesbitt@users.noreply.github.com>
2023-03-31 21:37:01 -0400

    trying to figure out how to load data from csv into data.db

--- load_db.js
+++ load_db.js
@@ -1,18 +1,44 @@
 const sqlite3 = require('sqlite3').verbose();
+const fs = require("fs");
 const { parse } = require("csv-parse");
 
 
 //create new connection to database
-let db = new sqlite3.Database('data.db', (err) => {
+  let db = new sqlite3.Database('data.db', (err) => {
     if (err) {
       return console.error(err.message);
     }
     console.log('Connected to the in-memory SQlite database.');
   });
 
+  
+  
+  
 
-  //create read stream for each CSV and load into tables
+  //create read stream for students CSV file 
 
+  fs.createReadStream("./csv_data/students.csv")
+    .pipe(parse({ delimiter: ",", from_line: 2 }))
+    .on("data", function (row) {
+         // for each row in csv file, make an insert statement.
+        let values = row.map((value) => '(?)').join(',');
+        let sql = 'INSERT INTO students_test VALUES ' + values;
+  
+        // insert into table using db connection
+        db.run(sql, row, function(err) {
+            if (err) {
+            return console.error(err.message);
+            }
+    console.log(`Rows inserted ${this.changes}`);
+  });
+    })
+    .on("error", function (error) {
+        console.log(error.message);
+    })
+    .on("end", function () {
+        console.log("Done");
+    });
+  
 
   //close connection to database
   db.close((err) => {
