0c0ba82fd4b5b6aaa3cbad2e1d343f07801959c0
Thomas Carriero <tcarriero@unc.edu>
2023-04-12 11:43:58 -0400

    fixed the navbar, frontend essentially done besides possible homepage, just needs data

--- react-rolex/src/routes/ClassTwo.js
+++ react-rolex/src/routes/ClassTwo.js
@@ -3,10 +3,12 @@ import React from 'react'
 import Calendar from '../components/Calendar';
 //import Dates from './components/Dates';
 import DataCard from '../components/DataCard';
+import Navbar from '../components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1 className='header-text'>Class Two</h1>
       <div className="container">
         <Calendar className='calendar' />
