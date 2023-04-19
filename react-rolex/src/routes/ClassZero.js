9c4e4bc6d9c68541e262e408ec946931a336ba1b
Kayla Nesbitt <77808103+kaylanesbitt@users.noreply.github.com>
2023-04-19 10:32:45 -0400

    it works in connection.js

--- react-rolex/src/routes/ClassZero.js
+++ react-rolex/src/routes/ClassZero.js
@@ -2,24 +2,27 @@ import React, { useState, useEffect } from "react";
 import { zeroData } from './zeroData'
 
 
+async function getZeroData(callback) {
+    const axios = require('axios')
+    axios.get('http://localhost:8000/0')
+        .then((response) => {
+            callback(response.data)
+        })
+        .catch(err => console.error(err))
+}
+
 
 function ClassZero() {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch(`http://localhost:8000/0`)
-            .then(response => response.json())
-            .then((usefulData) => {
-
-            })
-            .catch((e) => {
-                console.error(`An error occurred: ${e}`)
-            });
-    }, []);
-
-
-
+    const axios = require('axios')
+    var tuples = null
+    axios.get('http://localhost:8000/0')
+        .then((response) => {
+            console.log(response.data)
+            tuples = response.data
+        })
+        .catch(err => console.error(err))
+    return (
+        <div>{tuples}</div>
+    )
 }
 export default ClassZero
