b13585dcb6ff416448e4d9e1ce2fc6f50c72abe1
Kayla Nesbitt <77808103+kaylanesbitt@users.noreply.github.com>
2023-04-19 10:45:57 -0400

    this shit is so weird

--- react-rolex/src/routes/ClassZero.js
+++ react-rolex/src/routes/ClassZero.js
@@ -1,28 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { zeroData } from './zeroData'
-
-
-async function getZeroData(callback) {
-    const axios = require('axios')
-    axios.get('http://localhost:8000/0')
-        .then((response) => {
-            callback(response.data)
-        })
-        .catch(err => console.error(err))
-}
+import { getCourses, display } from './connection.js'
 
 
 function ClassZero() {
-    const axios = require('axios')
-    var tuples = null
-    axios.get('http://localhost:8000/0')
-        .then((response) => {
-            console.log(response.data)
-            tuples = response.data
-        })
-        .catch(err => console.error(err))
     return (
-        <div>{tuples}</div>
+        display(getCourses)
     )
 }
 export default ClassZero
--- react-rolex/src/routes/connection.js
+++ react-rolex/src/routes/connection.js
@@ -4,21 +4,18 @@ const getCourses = () => {
     const url = 'http://localhost:8000/0'
     axios.get(url)
         .then(response => {
-            //displayOutput(response)
-            response.data.message.forEach(element => {
-                console.log(element)
-            });
+            console.log(response.data.message)
             return response
         }).catch(err => console.log(err))
 }
 
-
-const displayOutput = (responseDisplay) => {
-    document.getElementById('responseDisplay').innerHTML = `
-<div>
-<h5>Status: ${responseDisplay.status}</h5>
-</div>
-`
+const display = (response) => {
+    return (`
+    <div>
+    <h1>Status: ${response.status} </h1>
+    </div>
+    `
+    )
 }
 
 getCourses(0)
