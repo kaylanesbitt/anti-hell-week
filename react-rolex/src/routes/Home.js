54e35c12b724c8f5afb81919fe0a71c25489124c
Aryan C <aryanc1027@gmail.com>
2023-04-19 19:38:53 -0400

    created a render function and printed function onto main mage

--- react-rolex/src/routes/Home.js
+++ react-rolex/src/routes/Home.js
@@ -6,17 +6,29 @@ import DataCard from '../components/DataCard';
 import Navbar from '../components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <h1 className='header-text'>Class One</h1>
-        <div className="container">
-          <Calendar className='calendar' />
-          {/* <DataCard className='deposits' /> */}
-          <DataCard className='deposits' />
+    return (
+        <div className="App">
+            <Navbar />
+            <h1 className='header-text'>{render()}</h1>
+            <div className="container">
+                <Calendar className='calendar' />
+                {/* <DataCard className='deposits' /> */}
+                <DataCard className='deposits' />
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
+function render() {
+    const data =[{"name":"test 1"},{"name":"test 2"}];
+    const listItems = data.map((d) => <li key={d.name}>{d.name}</li>);
+
+    return (
+
+    <div>
+        <h1>{listItems[0]}</h1>
+        <p> {listItems[1]}</p>
+    </div>
+    );
+}
 export default App;
